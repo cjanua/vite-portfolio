cfd124885f09635d7f42be45dbf0dc37bb01d2de
cjanua <139174067+cjanua@users.noreply.github.com>
2023-10-14 17:23:47 -0400

    some changes ig

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route } from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <script src="./node_modules/preline/dist/preline.js"></script> */}
     <App />
+
   </React.StrictMode>,
 )
